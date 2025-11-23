f3332eaa7c92ee51c6f8ad6b431f32ecfb906094
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 18:10:24 +0700

    commit 18

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,3 @@
-// lib/screens/splash_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:utspam_soald_if5b_3012310044/providers/auth_provider.dart';
@@ -8,7 +6,6 @@ import 'package:utspam_soald_if5b_3012310044/utils/theme.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -21,21 +18,16 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _navigateToNextScreen() async {
-    // Inisialisasi provider dan cek status login
     await Provider.of<AuthProvider>(context, listen: false)
         .loadUserFromStorage();
-
-    // Tunggu 2 detik untuk efek splash
     await Future.delayed(const Duration(seconds: 2));
-
     if (!mounted) return;
-
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
-    if (authProvider.isLoggedIn) {
-      Navigator.pushReplacementNamed(context, AppConstants.homeRoute);
-    } else {
-      Navigator.pushReplacementNamed(context, AppConstants.loginRoute);
-    }
+    Navigator.pushReplacementNamed(
+        context,
+        authProvider.isLoggedIn
+            ? AppConstants.homeRoute
+            : AppConstants.loginRoute);
   }
 
   @override
@@ -43,30 +35,18 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: AppTheme.lightColorScheme.primary,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.local_pharmacy,
-              size: 100,
-              color: AppTheme.lightColorScheme.onPrimary,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Apotek Mobile',
-              style: AppTheme.lightTheme.textTheme.headlineMedium?.copyWith(
-                color: AppTheme.lightColorScheme.onPrimary,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Solusi Kesehatan Anda',
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Icon(Icons.local_pharmacy,
+              size: 100, color: AppTheme.lightColorScheme.onPrimary),
+          const SizedBox(height: 20),
+          Text('Apotek Mobile',
+              style: AppTheme.lightTheme.textTheme.headlineMedium
+                  ?.copyWith(color: AppTheme.lightColorScheme.onPrimary)),
+          const SizedBox(height: 10),
+          Text('Solusi Kesehatan Anda',
               style: AppTheme.lightTheme.textTheme.bodyMedium?.copyWith(
-                color: AppTheme.lightColorScheme.onPrimary.withOpacity(0.8),
-              ),
-            ),
-          ],
-        ),
+                  color: AppTheme.lightColorScheme.onPrimary.withOpacity(0.8))),
+        ]),
       ),
     );
   }

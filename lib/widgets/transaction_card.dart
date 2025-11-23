4031aa355d52db957b6d9fc8b7c76f6b4f9fe1bd
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 18:09:40 +0700

    commit 17

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -1,5 +1,3 @@
-// lib/widgets/transaction_card.dart
-
 import 'package:flutter/material.dart';
 import 'package:utspam_soald_if5b_3012310044/models/transaction.dart';
 import 'package:utspam_soald_if5b_3012310044/utils/helpers.dart';
@@ -7,51 +5,38 @@ import 'package:utspam_soald_if5b_3012310044/utils/helpers.dart';
 class TransactionCard extends StatelessWidget {
   final Transaction transaction;
   final VoidCallback onTap;
-
-  const TransactionCard({
-    Key? key,
-    required this.transaction,
-    required this.onTap,
-  }) : super(key: key);
+  const TransactionCard(
+      {Key? key, required this.transaction, required this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: ListTile(
-        leading: Icon(
-          Icons.receipt_long,
-          color: transaction.status == TransactionStatus.canceled
-              ? Colors.grey
-              : Theme.of(context).colorScheme.primary,
-        ),
-        title: Text(
-          transaction.medicineName,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
+        leading: Icon(Icons.receipt_long,
+            color: transaction.status == TransactionStatus.canceled
+                ? Colors.grey
+                : Theme.of(context).colorScheme.primary),
+        title: Text(transaction.medicineName,
+            style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text('Tanggal: ${transaction.date}'),
         trailing: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text(
-              Helpers.formatCurrency(transaction.totalPrice),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
+            Text(Helpers.formatCurrency(transaction.totalPrice),
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary)),
             const SizedBox(height: 4),
-            Text(
-              transaction.status.name.toUpperCase(),
-              style: TextStyle(
-                fontSize: 12,
-                color: transaction.status == TransactionStatus.canceled
-                    ? Colors.red
-                    : Colors.green,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            Text(transaction.status.name.toUpperCase(),
+                style: TextStyle(
+                    fontSize: 12,
+                    color: transaction.status == TransactionStatus.canceled
+                        ? Colors.red
+                        : Colors.green,
+                    fontWeight: FontWeight.bold)),
           ],
         ),
         onTap: onTap,

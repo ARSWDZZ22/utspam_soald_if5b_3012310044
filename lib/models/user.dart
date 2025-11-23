eceffcadb7e3889ec2504066f6be85b4be7d6972
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 18:02:58 +0700

    commit 8

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,41 +1,29 @@
-// lib/models/user.dart
-
 class User {
-  final String fullName;
-  final String email;
-  final String phoneNumber;
-  final String address;
-  final String username;
-  final String password;
+  final String fullName, email, phoneNumber, address, username, password;
 
-  User({
-    required this.fullName,
-    required this.email,
-    required this.phoneNumber,
-    required this.address,
-    required this.username,
-    required this.password,
-  });
+  User(
+      {required this.fullName,
+      required this.email,
+      required this.phoneNumber,
+      required this.address,
+      required this.username,
+      required this.password});
 
-  Map<String, dynamic> toJson() {
-    return {
-      'fullName': fullName,
-      'email': email,
-      'phoneNumber': phoneNumber,
-      'address': address,
-      'username': username,
-      'password': password,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        'fullName': fullName,
+        'email': email,
+        'phoneNumber': phoneNumber,
+        'address': address,
+        'username': username,
+        'password': password
+      };
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      fullName: json['fullName'],
-      email: json['email'],
-      phoneNumber: json['phoneNumber'],
-      address: json['address'],
-      username: json['username'],
-      password: json['password'],
-    );
-  }
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        fullName: json['fullName'],
+        email: json['email'],
+        phoneNumber: json['phoneNumber'],
+        address: json['address'],
+        username: json['username'],
+        password: json['password'],
+      );
 }

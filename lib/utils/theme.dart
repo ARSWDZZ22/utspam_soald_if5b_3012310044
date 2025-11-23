fd480c0e6edb644c22c51817c29211160c332170
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 17:58:24 +0700

    commit 7

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,35 +1,18 @@
-// lib/utils/theme.dart
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ColorScheme lightColorScheme = ColorScheme.fromSeed(
-    seedColor: Colors.teal,
-    brightness: Brightness.light,
-  );
-
+      seedColor: Colors.teal, brightness: Brightness.light);
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     colorScheme: lightColorScheme,
-    textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-      headlineMedium: GoogleFonts.poppins(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: lightColorScheme.onSurface,
-      ),
-      bodyLarge: GoogleFonts.poppins(fontSize: 16),
-      bodyMedium: GoogleFonts.poppins(fontSize: 14),
-    ),
+    textTheme: GoogleFonts.poppinsTextTheme(),
     inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12.0),
-        borderSide: BorderSide(color: lightColorScheme.outline),
-      ),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12.0),
-        borderSide: BorderSide(color: lightColorScheme.primary, width: 2.0),
-      ),
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: BorderSide(color: lightColorScheme.primary, width: 2.0)),
       contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
@@ -37,9 +20,7 @@ class AppTheme {
         backgroundColor: lightColorScheme.primary,
         foregroundColor: lightColorScheme.onPrimary,
         padding: const EdgeInsets.symmetric(vertical: 16),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         textStyle: GoogleFonts.poppins(fontWeight: FontWeight.bold),
       ),
     ),

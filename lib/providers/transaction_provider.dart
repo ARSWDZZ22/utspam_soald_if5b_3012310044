5005b1cd239636c07179921156896c2b9eabde14
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 18:07:08 +0700

    commit 13

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -1,16 +1,14 @@
-// lib/providers/transaction_provider.dart
-
 import 'package:flutter/material.dart';
 import 'package:utspam_soald_if5b_3012310044/models/transaction.dart';
 import 'package:utspam_soald_if5b_3012310044/services/storage_service.dart';
 
 class TransactionProvider with ChangeNotifier {
   List<Transaction> _transactions = [];
-
   List<Transaction> get transactions => _transactions;
 
   Future<void> loadTransactions() async {
     _transactions = await StorageService.getAllTransactions();
+    _transactions.sort((a, b) => b.date.compareTo(a.date)); // Urutkan terbaru
     notifyListeners();
   }
 

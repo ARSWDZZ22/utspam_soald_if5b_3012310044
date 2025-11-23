bf5860090533209ea4245b808fb499b6157ca7fa
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 18:08:27 +0700

    commit 14

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,18 +1,15 @@
-// lib/widgets/custom_button.dart
-
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
   final bool isLoading;
-
-  const CustomButton({
-    Key? key,
-    required this.text,
-    required this.onPressed,
-    this.isLoading = false,
-  }) : super(key: key);
+  const CustomButton(
+      {Key? key,
+      required this.text,
+      required this.onPressed,
+      this.isLoading = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +22,7 @@ class CustomButton extends StatelessWidget {
                 height: 20,
                 width: 20,
                 child: CircularProgressIndicator(
-                  color: Colors.white,
-                  strokeWidth: 3,
-                ),
-              )
+                    color: Colors.white, strokeWidth: 3))
             : Text(text),
       ),
     );

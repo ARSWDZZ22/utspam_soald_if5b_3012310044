2c239de104a1bff1085ae071f2fd0a1c91dfb34d
ARSWDZZ22 <ARSWDZZ22@gmail.com>
2025-11-23 18:08:49 +0700

    commit 15

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,26 +1,21 @@
-// lib/widgets/custom_textfield.dart
-
 import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget {
   final TextEditingController controller;
   final String label;
-  final String? hintText;
   final String? Function(String?)? validator;
   final TextInputType keyboardType;
   final bool obscureText;
   final int maxLines;
-
-  const CustomTextfield({
-    Key? key,
-    required this.controller,
-    required this.label,
-    this.hintText,
-    this.validator,
-    this.keyboardType = TextInputType.text,
-    this.obscureText = false,
-    this.maxLines = 1,
-  }) : super(key: key);
+  const CustomTextfield(
+      {Key? key,
+      required this.controller,
+      required this.label,
+      this.validator,
+      this.keyboardType = TextInputType.text,
+      this.obscureText = false,
+      this.maxLines = 1})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +23,7 @@ class CustomTextfield extends StatelessWidget {
       padding: const EdgeInsets.only(bottom: 16.0),
       child: TextFormField(
         controller: controller,
-        decoration: InputDecoration(
-          labelText: label,
-          hintText: hintText,
-        ),
+        decoration: InputDecoration(labelText: label),
         validator: validator,
         keyboardType: keyboardType,
         obscureText: obscureText,
